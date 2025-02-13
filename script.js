2ea776448276dbd605d2aa1b15601ab640e3a320
Tanmoy Das <166496961+TANMOYDAS1234@users.noreply.github.com>
2025-02-14 00:15:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('mousemove', (e)=>{
+    document.body.style.setProperty('--x', e.clientX/window.innerWidth);
+    document.body.style.setProperty('--y', e.clientY/window.innerHeight);
+})
+
