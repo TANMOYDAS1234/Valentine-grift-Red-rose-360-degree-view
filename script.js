1d94034c08758318512362d8f87bbb78641d0fde
Tanmoy Das <166496961+TANMOYDAS1234@users.noreply.github.com>
2025-02-14 00:53:33 +0530

    script.js

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ document.addEventListener('mousemove', (e)=>{
     document.body.style.setProperty('--y', e.clientY/window.innerHeight);
 })
 
+document.addEventListener('touchmove', (e) => {
+    // Prevent default scrolling behavior
+    e.preventDefault();
+
+    // Access the first touch
+    const touch = e.touches[0];
+    
+    // Check if the touch exists and is valid
+    if (touch) {
+        document.body.style.setProperty('--x', touch.clientX / window.innerWidth);
+        document.body.style.setProperty('--y', touch.clientY / window.innerHeight);
+    }
+});
+
